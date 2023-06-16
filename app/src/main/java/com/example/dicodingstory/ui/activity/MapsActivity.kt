2e112fb0fed550c4f620fd9843cf547907a88632
Kurniawan Saputra <saputrak299@gmail.com>
2023-06-16 14:50:40 +0700

    Updating activity maps, etc

--- app/src/main/java/com/example/dicodingstory/ui/activity/MapsActivity.kt
+++ app/src/main/java/com/example/dicodingstory/ui/activity/MapsActivity.kt
@@ -1,20 +1,34 @@
 package com.example.dicodingstory.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.content.res.Resources
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
 import com.example.dicodingstory.R
-
+import com.example.dicodingstory.databinding.ActivityMapsBinding
+import com.example.dicodingstory.hawkstorage.HawkStorage
+import com.example.dicodingstory.viewmodel.MainViewModel
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
-import com.example.dicodingstory.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var token: String
+    private lateinit var mainViewModel: MainViewModel
 
     private lateinit var mMap: GoogleMap
+
     private lateinit var binding: ActivityMapsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +36,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        init()
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
@@ -40,10 +56,105 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.uiSettings.isZoomControlsEnabled = true
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+//        // Add a marker in Sydney and move the camera
+//        val sydney = LatLng(-34.0, 151.0)
+//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+//
+//        // Custom marker
+//        val dicodingSpace = LatLng(-6.8957643, 107.6338462)
+//        mMap.addMarker(
+//            MarkerOptions()
+//                .position(dicodingSpace)
+//                .title("Dicoding Space")
+//                .snippet("Batik Kumeli No.50")
+//                .icon(vectorToBitmap(resources, R.drawable.ic_location, Color.parseColor("#00aeff")))
+//                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+//        )
+//        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(dicodingSpace, 15f))
+
+        getMyLocation()
+        setMapStyle()
+        addManyMarker()
+    }
+
+    private fun init() {
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        setPref()
+    }
+
+    private fun setPref() {
+        val user = HawkStorage.instance(this).getUser()
+        token = user.loginResult?.token.toString()
+    }
+
+    private fun setMapStyle() {
+        try {
+            val success =
+                mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.maps_style))
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.")
+            }
+        } catch (exception: Resources.NotFoundException) {
+            Log.e(TAG, "Can't find style. Error: ", exception)
+        }
+    }
+
+    private val boundsBuilder = LatLngBounds.Builder()
+    private fun addManyMarker() {
+        mainViewModel.getStories(token)
+        mainViewModel.onFailure.observe(this) {
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+        }
+        mainViewModel.stories.observe(this) {
+            val error = it.error
+            if (error == false) {
+                val listStory = it.listStory
+                listStory?.forEach { story ->
+                    val latLng = LatLng(story?.lat!!, story.lon!!)
+                    mMap.addMarker(MarkerOptions().position(latLng).title(story.name))?.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.ic_location))
+                    boundsBuilder.include(latLng)
+                }
+
+                val bounds: LatLngBounds = boundsBuilder.build()
+                mMap.animateCamera(
+                    CameraUpdateFactory.newLatLngBounds(
+                        bounds,
+                        resources.displayMetrics.widthPixels,
+                        resources.displayMetrics.heightPixels,
+                        300
+                    )
+                )
+            }
+        }
+    }
+
+    private fun getMyLocation() {
+        if (ContextCompat.checkSelfPermission(
+                this.applicationContext,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            mMap.isMyLocationEnabled = true
+        } else {
+            requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    // Permission for access location
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                getMyLocation()
+            }
+        }
+
+    companion object {
+        private const val TAG = "MapsActivity"
     }
 }
